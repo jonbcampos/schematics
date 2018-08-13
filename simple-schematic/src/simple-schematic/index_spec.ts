f976622353eb0bbe70be4647d812813755a30a7a
Jonathan Campos <jonbcampos@gmail.com>
2018-08-13 09:29:01 -0500

    fixed up tests

--- simple-schematic/src/simple-schematic/index_spec.ts
+++ simple-schematic/src/simple-schematic/index_spec.ts
@@ -53,7 +53,40 @@ describe('simple-schematic', () => {
                 name: "test"
             }, appTree);
 
-            expect(tree.files).toEqual([]);
+            expect(tree.files).toEqual([
+                "/README.md",
+                "/angular.json",
+                "/package.json",
+                "/tsconfig.json",
+                "/tslint.json",
+                "/.editorconfig",
+                "/.gitignore",
+                "/projects/bar/browserslist",
+                "/projects/bar/karma.conf.js",
+                "/projects/bar/tsconfig.app.json",
+                "/projects/bar/tsconfig.spec.json",
+                "/projects/bar/tslint.json",
+                "/projects/bar/src/favicon.ico",
+                "/projects/bar/src/index.html",
+                "/projects/bar/src/main.ts",
+                "/projects/bar/src/polyfills.ts",
+                "/projects/bar/src/test.ts",
+                "/projects/bar/src/styles.css",
+                "/projects/bar/src/assets/.gitkeep",
+                "/projects/bar/src/environments/environment.prod.ts",
+                "/projects/bar/src/environments/environment.ts",
+                "/projects/bar/src/app/app.module.ts",
+                "/projects/bar/src/app/app.component.css",
+                "/projects/bar/src/app/app.component.html",
+                "/projects/bar/src/app/app.component.spec.ts",
+                "/projects/bar/src/app/app.component.ts",
+                "/projects/bar/src/app/test/test.spec.ts",
+                "/projects/bar/src/app/test/test.ts",
+                "/projects/bar-e2e/protractor.conf.js",
+                "/projects/bar-e2e/tsconfig.e2e.json",
+                "/projects/bar-e2e/src/app.e2e-spec.ts",
+                "/projects/bar-e2e/src/app.po.ts",
+            ]);
         });
 
     });
@@ -90,7 +123,40 @@ describe('simple-schematic', () => {
                 name: "test"
             }, appTree);
 
-            expect(tree.files).toEqual([]);
+            expect(tree.files).toEqual([
+                "/README.md",
+                "/angular.json",
+                "/package.json",
+                "/tsconfig.json",
+                "/tslint.json",
+                "/.editorconfig",
+                "/.gitignore",
+                "/src/favicon.ico",
+                "/src/index.html",
+                "/src/main.ts",
+                "/src/polyfills.ts",
+                "/src/test.ts",
+                "/src/styles.css",
+                "/src/browserslist",
+                "/src/karma.conf.js",
+                "/src/tsconfig.app.json",
+                "/src/tsconfig.spec.json",
+                "/src/tslint.json",
+                "/src/assets/.gitkeep",
+                "/src/environments/environment.prod.ts",
+                "/src/environments/environment.ts",
+                "/src/app/app.module.ts",
+                "/src/app/app.component.css",
+                "/src/app/app.component.html",
+                "/src/app/app.component.spec.ts",
+                "/src/app/app.component.ts",
+                "/src/app/test/test.spec.ts",
+                "/src/app/test/test.ts",
+                "/e2e/protractor.conf.js",
+                "/e2e/tsconfig.e2e.json",
+                "/e2e/src/app.e2e-spec.ts",
+                "/e2e/src/app.po.ts"
+            ]);
         });
 
     });
